88b713df077bd289cb85581767478087351a0822
SpoostyGuy <95593695+SpoostyGuy@users.noreply.github.com>
2022-09-19 14:52:34 -0400

    Update main.js

--- main.js
+++ main.js
@@ -50,7 +50,7 @@ async function ready() {
             var resposne = true
             tab.forEach(function(val2) {                
                 if (val2[Object.keys(val2)[0]] == 'present') {
-                    if (val.includes(Object.keys(val2)[0]) === false && val.charAt(0) === Object.keys(val2)[0]) {
+                    if (val.includes(Object.keys(val2)[0]) === false || val.charAt(0) === Object.keys(val2)[0]) {
                         resposne = false
                     }
                 }
@@ -65,7 +65,7 @@ async function ready() {
                     }
                 }
                  if (val2[Object.keys(val2)[1]] == 'present') {
-                    if (val.includes(Object.keys(val2)[1]) === false && val.charAt(1) === Object.keys(val2)[1]) {
+                    if (val.includes(Object.keys(val2)[1]) === false || val.charAt(1) === Object.keys(val2)[1]) {
                         resposne = false
                     }
                 }
@@ -80,7 +80,7 @@ async function ready() {
                     }
                 }
                  if (val2[Object.keys(val2)[2]] == 'present') {
-                    if (val.includes(Object.keys(val2)[2]) === false && val.charAt(2) === Object.keys(val2)[2]) {
+                    if (val.includes(Object.keys(val2)[2]) === false || val.charAt(2) === Object.keys(val2)[2]) {
                         resposne = false
                     }
                 }
@@ -95,7 +95,7 @@ async function ready() {
                     }
                 }
                  if (val2[Object.keys(val2)[3]] == 'present') {
-                    if (val.includes(Object.keys(val2)[3]) === false && val.charAt(3) === Object.keys(val2)[3]) {
+                    if (val.includes(Object.keys(val2)[3]) === false || val.charAt(3) === Object.keys(val2)[3]) {
                         resposne = false
                     }
                 }
@@ -110,7 +110,7 @@ async function ready() {
                     }
                 }
                  if (val2[Object.keys(val2)[4]] == 'present') {
-                    if (val.includes(Object.keys(val2)[4]) === false && val.charAt(4) === Object.keys(val2)[4]) {
+                    if (val.includes(Object.keys(val2)[4]) === false || val.charAt(4) === Object.keys(val2)[4]) {
                         resposne = false
                     }
                 }
